372c9339313647dd44f4366991112e32433c0910
Gustavo Oliveira <gustavooliveirarocha@hotmail.com>
2021-06-07 19:30:47 -0300

    Adicionar arquivos de projeto.

---
+++ ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using CP.Api.Gerenciamento.Procedimentos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP.Api.Gerenciamento.Procedimentos
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<Procedimento> Procedimentos { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public ApplicationContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProcedimentosDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Procedimento>().HasKey(p=>p.IDProcedimento);
+        }
+    }
+}
---
+++ Controller/ProcedimentoController.cs
@@ -0,0 +1,43 @@
+﻿using CP.Api.Gerenciamento.Procedimentos.Models;
+using CP.Api.Gerenciamento.Procedimentos.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP.Api.Gerenciamento.Procedimentos.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcedimentoController:ControllerBase
+    {
+        private readonly IProcedimentoRepository procedimentoRepository;
+
+        public ProcedimentoController(IProcedimentoRepository procedimentoRepository)
+        {
+            this.procedimentoRepository = procedimentoRepository;
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<Procedimento>> Get()
+        {
+            var retorno = procedimentoRepository.Consultar();
+            return Ok(retorno);
+        }
+
+        [HttpGet("{Procedimento}")]
+        [Authorize]
+        public ActionResult<IEnumerable<Procedimento>> Get([Required] string Procedimento)
+        {
+            var retorno = procedimentoRepository.ConsultarPorParametro(Procedimento);
+            return Ok(retorno);
+        }
+
+
+    }
+}
